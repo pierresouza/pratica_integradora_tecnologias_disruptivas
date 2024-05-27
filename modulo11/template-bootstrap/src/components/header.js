f1ba9ddee68c7a1698395e35fe6a268caa6d5925
pierresouza <pierre.s3@hotmail.com>
2024-05-27 15:42:29 -0300

    update

--- modulo11/template-bootstrap/src/components/header.js
+++ modulo11/template-bootstrap/src/components/header.js
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
- 
+import * as React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
 function Header() {
-    return (
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand>Gerenciamento de Projetos</Navbar.Brand>
-        </Container>
-      </Navbar>
-    );
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>Gerenciamento de Projetos</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 }
- 
+
 export default Header;
